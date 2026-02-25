52d4c666525f5f489479e0a3974c64c8caedf028
Brian Bauska <brianbauska@gmail.com>
2026-02-24 16:48:07 -0800

    Update and rename script.js to js/script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Poem text
-const kiplingPoem = `<p>Call the <span>roller</span> of big cigars,
+const kiplingPoem = `<p>Silus Gaming World says, 
+Call the <span>roller</span> of big cigars,
 The <span>muscular</span> one, and bid him <span>whip</span>
 In kitchen <span>cups</span> concupiscent <span>curds</span>.
 Let the <span>wenches</span> dawdle in such <span>dress</span>
@@ -7,7 +8,7 @@ As they are used to <span>wear</span>, and let the <span>boys</span>
 Bring <span>flowers</span> in last month's <span>newspapers</span>.
 <span>Let be be finale of seem</span>.
 <span>The only emperor is the emperor of ice-cream</span>.
-
+Continue from Silus Gaming World,
 Take from the <span>dresser</span> of <span>deal</span>,
 Lacking the three glass <span>knobs</span>, that sheet
 On which she embroidered <span>fantails</span> once
@@ -21,13 +22,13 @@ Let the <span>lamp</span> affix its <span>beam</span>.
 
 // Function to insert poem into divs
 function insertPoemIntoDivs() {
-	// Get all .text divs
-	const textDivs = document.querySelectorAll(".text");
+  // Get all .text divs
+  const textDivs = document.querySelectorAll(".text");
 
-	// Insert poem into all .text divs
-	textDivs.forEach((div) => {
-		div.innerHTML = kiplingPoem;
-	});
+  // Insert poem into all .text divs
+  textDivs.forEach((div) => {
+    div.innerHTML = kiplingPoem;
+  });
 }
 
 // Call the function when the DOM is fully loaded
@@ -35,11 +36,11 @@ document.addEventListener("DOMContentLoaded", insertPoemIntoDivs);
 
 const contentDiv = document.querySelector(".content");
 function adjustContentSize() {
-	const viewportWidth = window.innerWidth;
-	const baseWidth = 1000;
-	const scaleFactor =
-		viewportWidth < baseWidth ? (viewportWidth / baseWidth) * 0.8 : 1;
-	contentDiv.style.transform = `scale(${scaleFactor})`;
+  const viewportWidth = window.innerWidth;
+  const baseWidth = 1000;
+  const scaleFactor =
+    viewportWidth < baseWidth ? (viewportWidth / baseWidth) * 0.8 : 1;
+  contentDiv.style.transform = `scale(${scaleFactor})`;
 }
 window.addEventListener("load", adjustContentSize);
 window.addEventListener("resize", adjustContentSize);
