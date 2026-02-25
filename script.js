f69261db626ecee68968ee2d7c89db707ac8a679
Brian Bauska <brianbauska@gmail.com>
2026-02-24 16:38:25 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// Poem text
+const kiplingPoem = `<p>Call the <span>roller</span> of big cigars,
+The <span>muscular</span> one, and bid him <span>whip</span>
+In kitchen <span>cups</span> concupiscent <span>curds</span>.
+Let the <span>wenches</span> dawdle in such <span>dress</span>
+As they are used to <span>wear</span>, and let the <span>boys</span>
+Bring <span>flowers</span> in last month's <span>newspapers</span>.
+<span>Let be be finale of seem</span>.
+<span>The only emperor is the emperor of ice-cream</span>.
+
+Take from the <span>dresser</span> of <span>deal</span>,
+Lacking the three glass <span>knobs</span>, that sheet
+On which she embroidered <span>fantails</span> once
+And <span>spread</span> it so as to cover her <span>face</span>.
+If her horny <span>feet</span> protrude, they come
+To show how <span>cold</span> she is, and <span>dumb</span>.
+Let the <span>lamp</span> affix its <span>beam</span>.
+<span>The only emperor is the emperor of ice-cream</span>.
+
+     -Wallace Stevens</p>`;
+
+// Function to insert poem into divs
+function insertPoemIntoDivs() {
+	// Get all .text divs
+	const textDivs = document.querySelectorAll(".text");
+
+	// Insert poem into all .text divs
+	textDivs.forEach((div) => {
+		div.innerHTML = kiplingPoem;
+	});
+}
+
+// Call the function when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", insertPoemIntoDivs);
+
+const contentDiv = document.querySelector(".content");
+function adjustContentSize() {
+	const viewportWidth = window.innerWidth;
+	const baseWidth = 1000;
+	const scaleFactor =
+		viewportWidth < baseWidth ? (viewportWidth / baseWidth) * 0.8 : 1;
+	contentDiv.style.transform = `scale(${scaleFactor})`;
+}
+window.addEventListener("load", adjustContentSize);
+window.addEventListener("resize", adjustContentSize);
